d8331014d4c722b7e0bc62094c75c96076d64741
xxhmiguelxx <125937690+xxhmiguelxx@users.noreply.github.com>
2024-05-18 23:18:19 -0600

    C3

---
+++ proyectofinalweb/base_registros.sql
@@ -0,0 +1,13 @@
+Create database base_registros;
+
+use base_registros;
+
+CREATE TABLE registros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) not null,
+    email VARCHAR(50) not null
+);
+
+show tables;
+
+describe registros;
