31fdb5da5fcafc43a3617c4ac57138cfd6d6f91d
xxhmiguelxx <125937690+xxhmiguelxx@users.noreply.github.com>
2024-05-18 23:48:20 -0600

    C4

---
+++ proyectofinalweb/añadir.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "sql309.infinityfree.com";
+$username = "if0_36150780";
+$password = "jsB5zUCWUcu";
+$dbname = "if0_36150780_base_registros";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error: " . $conn->connect_error);
+}
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+
+
+$sql = "INSERT INTO registros (nombre, email) VALUES ('$nombre', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro exitoso:DDD";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
